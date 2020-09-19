c70409063bd33217dbdd31033130fff6ba1ad2dc
Crystal Dalsania <dalsania.crystal@gmail.com>
2020-09-19 15:58:58 -0400

    Defining variables, name of candidates, number of votes each candidate received, percentage of total votes, and total number of votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,18 @@
 # Import dependencies
 import os
 import csv
+
+election_data = os.path.join("election_data.csv")
+
+# Define variables
+# names of candidates
+candidates = []
+
+# number of votes each candidate receives
+num_votes = []
+
+# percentage of total votes each candidate garners 
+percent_votes = []
+
+# total number of votes
+total_votes = 0
